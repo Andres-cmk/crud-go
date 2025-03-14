0c73625ea863e2f21bd97d9a2d858541e7deff44
Andres-cmk <ramirezmontanaandresfelipe@gmail.com>
2025-03-13 20:53:51 -0500

    funcion editar/actualizar

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 
 var plantillaIndex, _ = template.ParseFiles("plantillas/index.html")
 var crear, _ = template.ParseFiles("plantillas/crear.html")
+var editar, _ = template.ParseFiles("plantillas/editar.html")
 
 func main() {
 	// manejador de endpoints
@@ -19,11 +20,41 @@ func main() {
 	http.HandleFunc("/crear", Crear)
 	http.HandleFunc("/insert", Insert)
 	http.HandleFunc("/borrar", Borrar)
+	http.HandleFunc("/editar", Editar)
+	http.HandleFunc("/actualizar", Actualizar)
 
 	log.Print("Listening on port 8080...")
 
 	// creacion de un servidor local.
 	_ = http.ListenAndServe(":8080", nil)
+}
+
+func Actualizar(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		dni := r.FormValue("dni")
+		nombre := r.FormValue("nombre")
+		apellido := r.FormValue("apellido")
+		numeroContacto := r.FormValue("numero")
+		correo := r.FormValue("email")
+
+		conexion, _ := db.ConexionDB()
+
+		idCliente, _ := strconv.Atoi(id)
+
+		modificarRegistro, err := conexion.Prepare("UPDATE cliente SET cli_DNI=?, cli_nombre=?, cli_apellido=?, cli_numContacto=?, cli_correo=? WHERE cli_id=?")
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		_, _ = modificarRegistro.Exec(dni, nombre, apellido, numeroContacto, correo, idCliente)
+
+		log.Println("Registro modificado con exito.")
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+
+	}
 }
 
 func Inicio(w http.ResponseWriter, r *http.Request) {
@@ -100,10 +131,39 @@ func Borrar(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err.Error())
 	}
-
 	_, _ = borrar.Exec(id)
-	log.Println("Registro guardado con exito.")
+	log.Println("Registro Eliminado con exito.")
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
+
+}
+
+func Editar(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	conexion, _ := db.ConexionDB()
+
+	registro, _ := conexion.Query("SELECT * FROM cliente WHERE cli_id = ? LIMIT 1", id)
+
+	cliente := models.Cliente{}
+
+	for registro.Next() {
+
+		var id int
+		var dni, name, lastName, numberContac, email string
+		err := registro.Scan(&id, &dni, &name, &lastName, &numberContac, &email)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		cliente.Id = id
+		cliente.Dni = dni
+		cliente.Nombre = name
+		cliente.Apellido = lastName
+		cliente.NumeroContacto = numberContac
+		cliente.Correo = email
+	}
+
+	_ = editar.Execute(w, cliente)
 
 }
